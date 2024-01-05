cd8384992ae6721f02336d4c192119afbb07cd08
Piyush Ahire <113412805+piyush-ahire@users.noreply.github.com>
2024-01-05 20:57:32 +0530

    Add files via upload

---
+++ DOM/a1.js
@@ -0,0 +1,9 @@
+const changebgRed=()=>{
+    document.body.style.background = "red"
+}
+
+
+
+let b = document.body;
+console.log("First child of b is:",b.firstChild);
+console.log("First child of b is:",b.firstElementChild); //returns valid child , only element will be returned
---
+++ attribute_methods/a2.js
@@ -0,0 +1,17 @@
+// let first = document.getElementById("first")
+// let a = first.getAttribute("class")
+//  console.log(a);
+
+//  console.log(first.hasAttribute("class"));
+//  console.log(first.hasAttribute("style"));
+
+ //first.setAttribute("hidden", "true")
+ //first.setAttribute("class","true sachin") //to set classes
+ //first.removeAttribute("class","true sachin") //to remove classes
+
+
+// console.log(first.attributes);
+
+console.log(first.dataset);
+console.log(first.dataset.game);
+
