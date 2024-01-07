0ad8d641ae8d683c104dc7b2b81a4414e085310e
Piyush Ahire <113412805+piyush-ahire@users.noreply.github.com>
2024-01-07 18:51:20 +0530

    Add files via upload

---
+++ inner_outer_html/a2.js
@@ -0,0 +1,18 @@
+ console.log(document.getElementsByTagName('span')[0]);
+
+ console.dir(document.getElementsByTagName('span')[0]); //gives tag
+
+ console.log(first.innerHTML);
+
+//  first.innerHTML = "hey, It is html"
+//  console.log(first.innerHTML);
+
+ //first.outerHTML = "<div>hello</div>"
+ console.log(first.outerHTML);
+
+ let a = document.body.firstChild.nodeValue //gives the data
+ console.log(a);
+
+ let c = document.body.textContent
+ console.log(c);
+
