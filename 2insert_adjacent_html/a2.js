80b226157b94229a23d2654ccaef7de6ca589502
Piyush Ahire <113412805+piyush-ahire@users.noreply.github.com>
2024-01-10 18:34:23 +0530

    Add files via upload

---
+++ 2insert_adjacent_html/a2.js
@@ -0,0 +1,8 @@
+// first.insertAdjacentHTML('beforebegin', '<div class = "test">beforebegin</div>'); //before start of the div
+// first.insertAdjacentHTML('beforeend', '<div class = "test">beforeend</div>');
+// first.insertAdjacentHTML('afterbegin', '<div class = "test">afterbegin</div>');
+// first.insertAdjacentHTML('afterend', '<div class = "test">afterend</div>');
+
+//node removal
+
+first.remove() //removes the the first id
