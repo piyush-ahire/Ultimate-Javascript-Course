f48021f8439ff2c68772fac9b7d5d61cee713226
Piyush Ahire <113412805+piyush-ahire@users.noreply.github.com>
2024-01-15 18:56:40 +0530

    Add files via upload

---
+++ 3_classname_classlist/a2.js
@@ -0,0 +1,14 @@
+first.className = "text-black red" // changes the class of the div with that id
+
+console.log(first.classList); //gives the list of all classes
+
+first.classList.remove("red") //to remove that class
+
+first.classList.add("red") //to add the class
+
+first.classList.toggle("red") //to add and remove the class
+first.classList.toggle("red") //to add and remove the class
+
+
+console.log(first.classList.contains("red"));
+
